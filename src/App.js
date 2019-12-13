2b1e7449df52281c203fe37f701ffb1b6fd89631
cww <819459233@qq.com>
2019-12-13 16:11:06 +0800

    页面

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import { Button } from 'antd';
-import './App.css';
-
-class App extends Component {
+//根应用
+import React, { Component } from "react";
+import { BrowserRouter, Route,Switch } from "react-router-dom";
+import Login from "./pages/login/login";
+import Admin from "./pages/admin/admin";
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+
+      <BrowserRouter>  
+      <Switch> 
+        <Route path="/login" Component={Login}></Route> 
+        <Route path="/" Component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
 
-export default App;
